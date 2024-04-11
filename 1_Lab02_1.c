dd7eb1d6e8e43df66846dad0421698fed5ceebd6
ayumyumi <164180342+ayumyumi@users.noreply.github.com>
2024-04-11 23:44:52 +0900

    1_Lab02_1.c

--- 1_Lab02_1.c
+++ 1_Lab02_1.c
@@ -4,8 +4,6 @@ int main(void) {
     
     long long A = 0, B = 0, C = 0;
 
-
-    //visual studio의 경우 scanf_s("%lld %lld %lld", &A, &B, &C);
     scanf("%lld %lld %lld", &A, &B, &C);
     
     printf("%lld", A + B + C);
