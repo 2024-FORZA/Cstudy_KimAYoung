d74b765920a51b072e6a3abb5aee2e1995239335
ayumyumi <164180342+ayumyumi@users.noreply.github.com>
2024-05-13 21:51:13 +0900

    Create 1_Lab04_2.c

---
+++ 1_Lab04_2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+long long int factorial(long long int n)
+{
+    if(n==0)
+        return 1;
+    else
+        return n*factorial(n-1);
+}
+
+int main()
+{
+    long long int num;
+
+    scanf("%lld",&num);
+
+    printf("%lld",factorial(num));
+
+    return 0;
+}
