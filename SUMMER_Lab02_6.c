55a34fbd7cacf3e5774c33b281e200a6648b0dfe
ayumyumi <164180342+ayumyumi@users.noreply.github.com>
2024-07-29 19:06:17 +0900

    Create SUMMER_Lab02_6.c

---
+++ SUMMER_Lab02_6.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int dp[41];
+int main()
+{
+    int N;
+    int M;
+    int i;
+    int result = 1;
+    int num;
+    int sub=0;
+    scanf("%d %d", &N, &M);
+    dp[0] = 1;
+    dp[1] = 1;
+    for (i = 2; i <=N; i++)
+        dp[i] = dp[i - 1] + dp[i - 2];
+    for (i = 0; i < M; i++)
+    {
+        scanf("%d", &num);
+        result *= dp[num-sub-1];
+        sub = num;
+    }
+    result *= dp[N-sub];
+    printf("%d", result);
+}
